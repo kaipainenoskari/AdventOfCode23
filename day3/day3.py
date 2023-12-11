08dbdc62558fc19f902bd5d1fb5beb3e8ba105e6
kaipainenoskari <oskari.kaipainen@hotmail.com>
2023-12-11 14:37:24 +0200

    day 3 part 2 done

--- day3/day3.py
+++ day3/day3.py
@@ -1,78 +1,60 @@
-from collections import OrderedDict
+from collections import Counter, defaultdict
 
-i = 0
-j = 0
-numbers = []
-rowLength = 0
-columnLength = 0
+def in_bound(x, y):
+    return 0 <= x < row_length and 0 <= y < column_length
 
-def inBound(x, y):
-    return x >= 0 and x < rowLength and y >= 0 and y < columnLength
-
-def checkAllSides(pos):
-    x = pos[0]
-    y = pos[1]
+def check_all_sides(pos):
+    x, y = pos
     for j in range(-1, 2):
         for i in range(-1, 2):
-            curX = x + i
-            curY = y + j
-            if (inBound(curX, curY)):
-                current = input[curY][curX]
-                if (not current.isnumeric() and current != '.'):
-                    return True
-    return False
-
-
+            cur_x, cur_y = x + i, y + j
+            if in_bound(cur_x, cur_y):
+                current = grid[cur_y][cur_x]
+                if current == '*':
+                    return (cur_x, cur_y)
+    return (-1, -1)
 
 with open('day3/input3.txt') as file:
-    input = [line.rstrip() for line in file]
-    for line in input:
-        if rowLength == 0:
-            rowLength = len(line)
-        addedList = []
-        for char in line:
-            if (char.isnumeric()):
-                addedList.append((int(char), (i, j)))
-            elif (len(addedList) > 0):
-                numbers.append(addedList)
-                addedList = []
-            else:
-                addedList = []
-            i += 1
-        if (len(addedList) > 0):
-            numbers.append(addedList)
-        i = 0
-        j += 1
-    columnLength = j
+    grid = [line.rstrip() for line in file]
 
-#print(numbers)
+numbers = []
+row_length, column_length = len(grid[0]), len(grid)
 
-numbersToSum = []
+for j, line in enumerate(grid):
+    added_list = []
+    for i, char in enumerate(line):
+        if char.isnumeric():
+            added_list.append((char, (i, j)))
+        elif len(added_list) > 0:
+            numbers.append(added_list)
+            added_list = []
+    if len(added_list) > 0:
+        numbers.append(added_list)
 
-for longNumber in numbers:
-    for number, pos in longNumber:
-        #print(number, pos)
-        if (checkAllSides(pos)):
-            numbersToSum.append(longNumber)
+numbers_to_sum = []
+
+for long_number in numbers:
+    for number, pos in long_number:
+        current = check_all_sides(pos)
+        if current != (-1, -1):
+            numbers_to_sum.append((long_number, current))
             break
 
-numbersToSum = list(map(lambda x: list(map(lambda y: y[0], x)), numbersToSum))
+numbers_to_sum = [(int(''.join(map(lambda y: y[0], x[0]))), x[1]) for x in numbers_to_sum]
 
-print(numbersToSum)
+# Count occurrences of second values
+second_value_counts = Counter(item[1] for item in numbers_to_sum)
 
-sum = 0
-for numberList in numbersToSum:
-    x = 1
-    numberList.reverse()
-    add = 0
-    for number in numberList:
-        add += number * x
-        x *= 10
-    print(add)
-    sum += add
+# Filter tuples where the second value appears at least 2 times
+filtered_list = [item for item in numbers_to_sum if second_value_counts[item[1]] >= 2]
 
-print(sum)
+# Use a defaultdict to store products for each unique second value
+product_dict = defaultdict(lambda: 1)
 
-# List of dicts number -> coordinate
-# def inBound(pos: coordinate) -> boolean checks if the pos is inbound 
-# def check(pos: coordinate) -> boolean checks if there are symbols around the pos
+# Calculate products for each unique second value
+for item in filtered_list:
+    product_dict[item[1]] *= item[0]  # Accumulate the product
+
+products = sum(product_dict.values())
+
+print(products)
